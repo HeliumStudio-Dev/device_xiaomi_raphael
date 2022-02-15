71a68368e0781519a20b17a58747ba6a31c60ed7
Robert Shih <robertshih@google.com>
2022-02-15 17:07:30 +0000

    raphael: Switch DRM HAL to AIDL
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I431554dcbef014f8235f048ee062a218a2131f9c

--- device.mk
+++ device.mk
@@ -200,7 +200,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Fastbootd
 PRODUCT_PACKAGES += \
