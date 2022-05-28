13d9782d8a005696f8eeb1bae767d9303010a71d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:08:59 +0200

    raphael: Move to common Xiaomi fingerprint HIDL
    Change-Id: I20a7daf4cf3e5598327c332d2958f035193082da

--- device.mk
+++ device.mk
@@ -202,7 +202,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_raphael
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 # FM
 PRODUCT_PACKAGES += \
