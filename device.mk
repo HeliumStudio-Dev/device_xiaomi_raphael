ca2309be9f108e85c511c21c02a91a97d1c6b564
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-23 15:55:14 +0200

    raphael: Implement UDFPS handler
    Change-Id: I7afff305140639d38ebb8bc51fbe790a1636db9e

--- device.mk
+++ device.mk
@@ -204,6 +204,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
+PRODUCT_PACKAGES += \
+    libudfpshandler
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
