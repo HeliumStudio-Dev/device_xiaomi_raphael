d7504e80d38f5e5be21747211fac709354ec7d6d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-09 15:49:40 +0200

    raphael: udfps: Update handler methods
    Change-Id: I7778ba69dcdf2843d3c11f5bd0ec92f1887f7395

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -90,6 +90,14 @@ class XiaomiRaphaelUdfpsHandler : public UdfpsHandler {
     void onFingerUp() {
         // nothing
     }
+
+    void onAcquired(int32_t /*result*/, int32_t /*vendorCode*/) {
+        // nothing
+    }
+
+    void cancel() {
+        // nothing
+    }
   private:
     fingerprint_device_t *mDevice;
 };
