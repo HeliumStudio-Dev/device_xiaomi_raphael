338d337bfd43e1230b9823eb0e509dbea1dbede8
Michael Bestas <mkbestas@lineageos.org>
2025-01-27 12:44:55 +0330

    raphael: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -279,6 +279,11 @@ PRODUCT_COPY_FILES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Popup Camera
 PRODUCT_PACKAGES += \
     XiaomiPopupCamera
