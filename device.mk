d3b0465099769a91b1d9f16671d5349fedea0f45
Michael Bestas <mkbestas@lineageos.org>
2025-04-05 19:46:15 +0300

    raphael: Switch to common lineage light HAL
    Change-Id: I0cea8f0d39aa0d33b8f188c286eb0775cdd2020f

--- device.mk
+++ device.mk
@@ -242,7 +242,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
