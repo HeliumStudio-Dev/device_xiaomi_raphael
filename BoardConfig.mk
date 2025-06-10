55b56b84abdb82dd3fefd6f12ba1e91d34ce6119
Bruno Martins <bgcngm@gmail.com>
2025-06-10 18:44:42 +0200

    raphael: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,9 +85,6 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += raphael
 ODM_MANIFEST_RAPHAEL_FILES := $(DEVICE_PATH)/manifest_nfc.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_raphael
-
 # Media
 TARGET_USES_ION := true
 
