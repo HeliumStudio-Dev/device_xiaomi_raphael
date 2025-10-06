0a38ec0b02c7a09aacb2b2cf4b75716ce4a16c52
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-10-06 21:43:22 +0800

    raphael: Switch biometrics/motor interfaces to common source
    Change-Id: I5dd3ad7d57ecb340988fe239f05db464bd89e489

--- interfaces/updates-makefiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
-
-do_makefiles_update \
-  "vendor.goodix:device/xiaomi/raphael/interfaces/goodix"
-
-do_makefiles_update \
-  "vendor.xiaomi:device/xiaomi/raphael/interfaces/xiaomi"
