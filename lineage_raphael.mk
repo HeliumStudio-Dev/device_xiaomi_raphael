033ab799f703e3e72225450777b7f3eccdd22ffd
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-10-06 19:35:44 +0800

    raphael: Revert to LineageOS tree
    Change-Id: I1504d2cb40de81f66bc2d9ac453a356b9736cfbe

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -12,18 +12,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_raphael
+PRODUCT_NAME := lineage_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi K20 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-
-TARGET_INCLUDE_PIXEL_CHARGER := true
-DEVICE_MAINTAINER := TH779, kubersharma001
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
