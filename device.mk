955671d3d1e63e04290477d1a840da32f49539a3
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-10-07 21:02:35 +0800

    raphael: Build missing libprotobuf-cpp-full package
    Change-Id: Ia8f4e50a64cb3da0cc14e5317fde45ffc07104b7

--- device.mk
+++ device.mk
@@ -391,6 +391,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
+    libprotobuf-cpp-full \
     librmnetctl \
     libxml2
 
