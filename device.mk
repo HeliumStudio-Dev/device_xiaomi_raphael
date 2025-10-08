1a387f7933d3a54f473e759aa69b6de9189bbce2
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-10-08 20:34:57 +0800

    raphael: Migrate to retrofit dynamic partitions
    * Android 14 no longer support BOARD_BUILD_SYSTEM_ROOT_IMAGE flag, which means
      we need to switch to two-stage init mounting.
    * Meanwhile, retrofit dynamic partitions also requires two-stage init mounting,
      why not just migrate to it to get more free space on system partitions?
    * Build ext4 SSI partitions images, erofs treble partitions images.
    * Also, switch data encryption method to FBEv2 and enable casefolding support.
    
    * Note: Only set PRODUCT_USE_DYNAMIC_PARTITIONS flag to prevent a build error.
    
    * References listed below:
     - https://source.android.com/docs/core/ota/dynamic_partitions/implement#upgrading-devices
     - https://github.com/Mi-Thorium/android_device_xiaomi_Mi439/commit/0795d4fd24d8dbc144163ceef49f143958fc2ac3
     - https://github.com/xiaomi-sm8150-devs/android_device_xiaomi_sm8150-common/commit/57ea6d9b880583efe026bcfb9703e366395f035a
    
    Co-authored-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Co-authored-by: Yumi Yukimura <me.cafebabe@gmail.com>
    Change-Id: Ia1c85d27eda1d66d059fcc610092ed5c9ef1b237

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/raphael/raphael-vendor.mk)
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
@@ -209,6 +212,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
@@ -370,6 +377,9 @@ PRODUCT_PACKAGES += \
 # Parts
 PRODUCT_PACKAGES += \
     XiaomiParts
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
 PRODUCT_PACKAGES += \
