39c4eb5c5aea609415ff688a765723c2e96fc6eb
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-11-23 03:48:10 +0800

    raphael: Build stub Megvii interface libs from common source
    Change-Id: Ia56fb561f133806bec77e9ea34b3d796b81e2431

--- device.mk
+++ device.mk
@@ -154,6 +154,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
     vendor.xiaomi.hardware.motor@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    libMegviiFacepp-0.5.2 \
+    libmegface
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
