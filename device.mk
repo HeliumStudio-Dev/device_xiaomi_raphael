db77aa0e57b3d60f7d87f0873a2f34eab1a60444
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-12-11 20:33:14 +0800

    raphael: Bring back dummy libqti-perfd-client
    Change-Id: I2019d200412cb4a7feef1270dbedea4e36350175

--- device.mk
+++ device.mk
@@ -31,7 +31,6 @@ PRODUCT_SOONG_NAMESPACES += \
     bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
 # Boot animation
