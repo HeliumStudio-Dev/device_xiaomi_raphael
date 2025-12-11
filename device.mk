4df6bb3ce8b2e61718851b83e0c367be48ebf9d8
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-12-11 20:12:12 +0800

    raphael: light: Rebrand to raphael
    Change-Id: I29321cf4421b1717ed6f1a72f37aab8b3692fbac

--- device.mk
+++ device.mk
@@ -257,7 +257,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.lineage
+    android.hardware.light-service.raphael
 
 # Media
 PRODUCT_COPY_FILES += \
