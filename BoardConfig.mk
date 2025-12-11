19aafe7427421ad80c1fead0c0a860ed0e191276
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-12-11 20:23:48 +0800

    raphael: Import common AIDL power HAL from LineageOS
    * HEAD: https://github.com/LineageOS/android_hardware_lineage_interfaces/commit/206625e9e79b4a0892e9ed3be0cef2c7780576d2
    
    Change-Id: Ie8dd49c700cda73327dd32d6b00aea5870e61dd5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,6 @@ VENDOR_SECURITY_PATCH := 2021-07-01
 
 # SELinux
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
-include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
