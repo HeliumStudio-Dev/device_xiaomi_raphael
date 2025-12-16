c17aca04a468b282d0e21c1a940c55c7f234bd02
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-12-16 18:40:22 +0800

    raphael: Move properties to a dedicated directory
    * This makes the tree look prettier and for ease of management.
    
    Change-Id: Ie62d2a44364d0833deae8798d0d91b666e3903e6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,11 +130,11 @@ BOARD_RAPHAEL_DYNAMIC_PARTITIONS_PARTITION_LIST := $(ALL_PARTITIONS)
 -include vendor/lineage/config/BoardConfigReservedSize.mk
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/properties/system_ext.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
