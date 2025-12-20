3d3ce4903637ba5e49157683c2d57276d26ab374
Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>
2025-12-20 14:50:19 +0800

    raphael: Import media_codecs.xml from coral
    * HEAD: https://github.com/LineageOS/android_device_google_coral/commit/07f9332c03dc4fa864d6bb38e95490499dd8baa0
    
    Change-Id: I6fb6a2a8029497f19bf267cc77cb12a5d3fc706e

--- device.mk
+++ device.mk
@@ -310,6 +310,7 @@ PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
